880436eef8f1ca3d9c0e89520872bef3a5709445
Pasan <pasankg@gmail.com>
2023-08-06 00:27:29 +1000

    Create ArticleApp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import { ArticlesApp } from './ArticlesApp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className="App">      
+      <ArticlesApp/>
     </div>
   );
 }
